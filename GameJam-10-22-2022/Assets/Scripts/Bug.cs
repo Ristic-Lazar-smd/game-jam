f420197fb1dc86bf8f5e7e792debdda4e7c66ec6
aleksam02 <115112258+aleksam02@users.noreply.github.com>
2022-10-22 14:23:59 +0200

    aki

---
+++ GameJam-10-22-2022/Assets/Scripts/Bug.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bug : MonoBehaviour
+{
+    public GameObject bug;
+    private GameObject newBug;
+    private float randomXposition, randomYposition;
+    private Vector3 spawnPosition;
+    private float spawnTime;
+    private bool bugSpawn = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        while(bugSpawn == true)
+        {
+            spawnTime = Random.Range(1, 2);
+            StartCoroutine(ExampleCoroutine());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+        yield return new WaitForSeconds(spawnTime);
+        randomXposition = Random.Range(-8f, 8f);
+        randomYposition = Random.Range(-4f, 4f);
+        spawnPosition = new Vector3(randomXposition, randomYposition, 0f);
+        newBug = Instantiate(bug, spawnPosition, Quaternion.identity);
+        Debug.Log("AAAAAAAA");
+    }
+}
